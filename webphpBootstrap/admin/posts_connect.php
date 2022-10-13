23aefb55fb7f5c1ecd450d0ff1f23cae09de720c
Le Chi Hien <102401402+hiendapda@users.noreply.github.com>
2022-10-13 16:24:29 +0700

    Add files via upload

---
+++ webphpBootstrap/admin/posts_connect.php
@@ -0,0 +1,4 @@
+<?php
+$conn = mysqli_connect("localhost", "root", "", "data1");
+mysqli_set_charset($conn, 'UTF8');
+?>
---
+++ webphpBootstrap/connect.php
@@ -0,0 +1,1 @@
+<?php $conn = mysqli_connect("localhost", "root", "", "data1"); mysqli_set_charset($conn, 'UTF8'); ?>
